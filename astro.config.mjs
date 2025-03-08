5c73f2af53aa424ecf2f0e3ec3111733ec0a601d
toxac <derocon@gmail.com>
2025-03-08 21:41:19 +0530

    init

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+
+import icon from "astro-icon";
+
+import netlify from "@astrojs/netlify";
+
+// https://astro.build/config
+export default defineConfig({
+  output:'server',
+  integrations: [mdx(), sitemap(), tailwind(), icon()],
+  adapter: netlify(),
+});
