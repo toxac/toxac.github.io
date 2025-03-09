fc96cdd957fbb45bbc854040e7851c907a5cb0fe
toxac <derocon@gmail.com>
2025-03-09 22:39:33 +0530

    rebase

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,9 @@ import tailwind from "@astrojs/tailwind";
 
 import icon from "astro-icon";
 
-import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  output:'server',
+  output:'static',
   integrations: [mdx(), sitemap(), tailwind(), icon()],
-  adapter: netlify(),
 });
